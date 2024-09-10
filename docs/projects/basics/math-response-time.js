001b07fa6e041d8acab2899595150c310f20df05
Katie Arrambide <katiearrambide@college.harvard.edu>
2024-09-10 17:05:48 -0400

    task set q6

---
+++ docs/projects/basics/math-response-time.js
@@ -0,0 +1,8 @@
+alert('In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.');
+let response1 = prompt('What is 5 + 5?');
+alert('You answered:' + response1);
+let response2 = prompt('What is 5 + 6?');
+alert('You answered:' + response2);
+let response3 = prompt('What is 5 + 7?');
+alert('You answered:' + response3);
+alert('Thank you for your participation!');
