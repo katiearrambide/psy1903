31f8acef7638b41e866e0bb4eb28e7f85a307b03
Katie Arrambide <katiearrambide@college.harvard.edu>
2024-09-10 14:59:58 -0400

    week 2 practice notes from videos

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+let welcomeMessage = `
+Welcome to our ` + experiment + `experiment.
+Please read the instructions carefully
+`;
+console.log(welcomeMessage);
+
+// Define a variable to hold our experiment name
+let experiment = 'Stroop';
+console.log(typeof experiment); // string
+console.log(experiment.charAt(0)); // 'S'
+
+
+let trialCountMax = 20;
+console.log(trialCountMax.charAt(0));
+console.log(typeof trialCountMax); // number
+
+// TODO: randomize colors
+let colors = ['red', 'green', 'blue'];
+
+alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+
+// At the halfway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+console.log(halfWayCount);
+
+let correct = true;
+console.log(typeof correct);
+
+console.log(10 > 15); // false
