3c1505adc0f318dc10e4feb1a7217936c102b3db
Katie Arrambide <katiearrambide@college.harvard.edu>
2024-09-17 16:51:43 -0400

    task set 3 q 8

--- docs/projects/week3-misc-practice/app.js
+++ docs/projects/week3-misc-practice/app.js
@@ -1,22 +1,33 @@
 
-let num1 = Math.floor(Math.random() * 10) + 1;
-let num2 = Math.floor(Math.random() * 10) + 1;
+//let num1 = Math.floor(Math.random() * 10) + 1;
+//let num2 = Math.floor(Math.random() * 10) + 1;
 
-let response = prompt('What is ' + num1 + ' + ' + num2 + ' ? ');
+//let response = prompt('What is ' + num1 + ' + ' + num2 + ' ? ');
 
-let feedback = '';
+//let feedback = '';
 
-if (response == num1 + num2) {
-    feedback = 'Correct!';
-} else if (response == (num1 + num2) + 1 || (num1 + num2) - 1) {
-    feedback = 'You were close!';
-} else {
-    feedback = 'Incorrect.';
-}
+//if (response == num1 + num2) {
+feedback = 'Correct!';
+//} else if (response == (num1 + num2) + 1 || (num1 + num2) - 1) {
+feedback = 'You were close!';
+//} else {
+feedback = 'Incorrect.';
+//}
 
-alert(feedback + ' The expected answer is ' + (num1 + num2));
+//alert(feedback + ' The expected answer is ' + (num1 + num2));
 
 
 
 //let num1 = Math.floor(Math.random() * 10) + 1;
 //let num2 = Math.floor(Math.random() * 10) + 1;
+
+let age = prompt('How old are you?');
+if (age < 12) {
+    alert('Child');
+}
+if (age >= 12 && age < 18) {
+    alert('Teenager');
+}
+if (age >= 18) {
+    alert('Adult');
+}
