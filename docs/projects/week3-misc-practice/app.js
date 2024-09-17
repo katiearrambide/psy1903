2e885ea3c7f8e124dcbc9feec58bf0ce646f94f5
Katie Arrambide <katiearrambide@college.harvard.edu>
2024-09-17 17:02:19 -0400

    task set 3 q 9

--- docs/projects/week3-misc-practice/app.js
+++ docs/projects/week3-misc-practice/app.js
@@ -21,13 +21,22 @@ feedback = 'Incorrect.';
 //let num1 = Math.floor(Math.random() * 10) + 1;
 //let num2 = Math.floor(Math.random() * 10) + 1;
 
-let age = prompt('How old are you?');
-if (age < 12) {
-    alert('Child');
+//let age = prompt('How old are you?');
+//if (age < 12) {
+//alert('Child');
+//}
+//if (age >= 12 && age < 18) {
+//alert('Teenager');
+//}
+//if (age >= 18) {
+//alert('Adult');
+//}
+
+let response = prompt('Please enter a whole number.');
+
+if (response % 2 == 0) {
+    alert('The number you entered was even.');
 }
-if (age >= 12 && age < 18) {
-    alert('Teenager');
-}
-if (age >= 18) {
-    alert('Adult');
+if (response % 2 !== 0) {
+    alert('The number you entered was odd. ');
 }
