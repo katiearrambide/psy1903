c9fd4b73f31b97c896e2b973da2874d8e81427bd
Katie Arrambide <katiearrambide@college.harvard.edu>
2024-09-24 17:31:59 -0400

    week 4 task set q 5

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -8,19 +8,33 @@
 
 // Question 4
 
-function convertTemp(temp, convertTo) {
-    if (convertTo == 'c') {
-        return (temp - 32) / 1.8;
-    }
+//function convertTemp(temp, convertTo) {
+//if (convertTo == 'c') {
+//return (temp - 32) / 1.8;
+// }
 
-    if (convertTo == 'f') {
-        return (temp * 1.8) + 32;
-    }
-}
+//if (convertTo == 'f') {
+//return (temp * 1.8) + 32;
+//}
+//}
 
 //console.log(convertTemp(10, 'c'));
 //console.log(convertTemp(10, 'f'));
 
+//Question 5
 
+let results = [];
+
+function getWordLengths(words) {
+    for (let word of words) {
+        results.push(word.length)
+    }
+    return (results);
+}
+
+
+let words = ['apple', 'banana', 'cherry', 'pear', 'grape'];
+
+console.log(getWordLengths(words));
 
 
