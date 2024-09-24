de7b5df39551f01141226748435210c0b649f372
Katie Arrambide <katiearrambide@college.harvard.edu>
2024-09-24 16:26:19 -0400

    task set 4 q 4

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,11 +1,25 @@
 // Question 3
 
-function celsiusToFarenheit(celsius) {
-    return (celsius * 1.8) + 32;
-}
+//function celsiusToFarenheit(celsius) {
+//return (celsius * 1.8) + 32;
+//}
 
 //console.log(celsiusToFarenheit(10));
 
+// Question 4
+
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'c') {
+        return (temp - 32) / 1.8;
+    }
+
+    if (convertTo == 'f') {
+        return (temp * 1.8) + 32;
+    }
+}
+
+//console.log(convertTemp(10, 'c'));
+//console.log(convertTemp(10, 'f'));
 
 
 
