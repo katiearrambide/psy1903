c70025c8cbd200164dc29526b10213c92aa17b37
Katie Arrambide <katiearrambide@college.harvard.edu>
2024-10-01 16:28:21 -0400

    task set 5 q 3

---
+++ docs/projects/mrt-jspsych/conditions.js
@@ -0,0 +1,21 @@
+// Initialize an empty array you will populate with your conditions
+let conditions = [];
+
+for (let i = 0; i < 3; i++) {
+    let num1 = getRandomNumber(1, 10);
+    let num2 = getRandomNumber(1, 10);
+    let answer = (num1 + num2);
+
+    conditions.push({ 'num1': num1, 'num2': num2, 'answer': answer });
+}
+
+
+// Output the resulting conditions array to make sure it is set up correctly
+console.log(conditions);
+
+
+// generate random 1-10
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
